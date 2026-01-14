3aaacc9be063f72cec8c5c7b60d0da47e62f63bf
CaroGil <dcgil83@gmail.com>
2026-01-13 16:17:21 -0800

    Primer commit: creación del proyecto

---
+++ fit_life/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:fit_life/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fit Life',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const LoginScreen(),
+    );
+  }
+}
